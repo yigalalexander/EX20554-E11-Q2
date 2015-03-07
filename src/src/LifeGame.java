c7c04072d6ca6b52880f1fe78a1ee1e8a583bbd6
yigalalexander <me@yigalalexander.com>
2015-03-07 21:11:35 +0200

    RandomizeMatrix

--- src/src/LifeGame.java
+++ src/src/LifeGame.java
@@ -3,19 +3,39 @@ package src;
 import javax.swing.JOptionPane;
 
 public class LifeGame {
+	
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Get matrix size
-		// Init a new matrix object
+		String buffer;
+		int width, length, userChoice;
+		LifeMatrix earth;
+		String[] buttons = { "Continue Game", "Exit", "Wipe all life" };
 		
+
+		// Get matrix size
+		buffer=JOptionPane.showInputDialog(null, "Enter the width of the matrix");
+		width=Integer.parseInt(buffer);
+		buffer=JOptionPane.showInputDialog(null, "Enter the length of the matrix");
+		length=Integer.parseInt(buffer);
+		
+		// Init a new matrix object
+		earth = new LifeMatrix(width, length);
 		
 		do { // while the result of confirmdialog is 0 play
-			JOptionPane.showMessageDialog(null, "Playing");
-		} while (JOptionPane.showConfirmDialog(null, "Do you want to play again?")==0);
+			JOptionPane.showMessageDialog(null, earth.toString());
+			
+			userChoice=JOptionPane.showOptionDialog(null, "What do you want to do?", "LifeGame",
+			        JOptionPane.PLAIN_MESSAGE, 0, null, buttons, buttons[2]);
+			if (userChoice==2) {
+				earth.RandomizeMatrix();
+			} else if (userChoice==0) {
+				earth.StepGeneration(1);
+			}			
+		} while (userChoice!=1);
 		return;
 		
 
--- src/src/LifeMatrix.java
+++ src/src/LifeMatrix.java
@@ -1,5 +1,51 @@
 package src;
 
+import java.util.Random;
+
 public class LifeMatrix {
+	
+	private boolean _matrix[][];
+	private int _width;
+	private int _length;
+	
+	/**
+	 * @param width Width of the new matrix
+	 * @param length Length of the new matrix
+	 */
+	public LifeMatrix (int width, int length){
+		_matrix = new boolean[width][length];
+		_width=width;
+		_length=length;
+		this.RandomizeMatrix();
+	}
+	
+	public void RandomizeMatrix() {
+		Random randomGenerator = new Random();
+		for (int len=0; len<_length; len++) {
+			for (int wid=0; wid<_width; wid++) {
+				_matrix[wid][len]=randomGenerator.nextBoolean();
+			}
+		}
+	}
+	
+	public void StepGeneration(int count) {
+		
+	}
+	
+	public String toString() {
+		
+		String temp = new String();
+		for (int len=0; len<_length; len++) {
+			
+
+			for (int wid=0; wid<_width; wid++) {
+				temp+=_matrix[wid][len]==true?"+":"-";
+				
+			}
+			temp+="\n";
+		}
+		
+		return temp;
+	}
 
 }
